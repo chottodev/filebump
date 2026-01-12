6cee6fcd636d4b6a5a0d8977a4a3e7418ff3b4bd
sergey <110@mobilon.ru>
2026-01-12 18:06:41 +0700

    fix

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,6 @@ WORKDIR /usr/src/app
 COPY package.json /usr/src/app/
 COPY package-lock.json /usr/src/app/
 
-RUN npm set registry http://npm.services.mobilon.ru
-
 RUN npm install
 
 COPY . /usr/src/app
--- packages/file-api/src/config/index.js
+++ packages/file-api/src/config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   
   baseUrl: process.env.FILE_API_BASE_URL || 'http://localhost:3007',
-  uploadDir: process.env.FILE_API_UPLOAD_DIR || '/tmp/uploads',
+  uploadDir: process.env.FILE_STORAGE_DIR || '/tmp/uploads',
   
   authHeader: process.env.FILE_API_AUTH_HEADER || 'X-API-Key',
   keys: process.env.FILE_API_KEYS ? process.env.FILE_API_KEYS.split(',') : ['testKey1', 'testKey2'],
